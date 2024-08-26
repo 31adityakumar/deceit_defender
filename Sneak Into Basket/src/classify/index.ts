9e4dc82661b059e53ae42cc0dfd1289ea589917f
Aditya <kumar.31aditya@gmail.com>
2024-08-26 15:02:03 +0530

    DEL: Sneaking Basket Algo

--- Sneak Into Basket/src/classify/index.ts
+++
@@ -1,9 +0,0 @@
-import * as puppeteer from 'puppeteer';
-
-const classify = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`file:///${process.cwd()}/index.html`);
-};
-
-export default classify;
